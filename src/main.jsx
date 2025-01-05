92c0cdd16c4b5523702eb8d6e07192e5fd1d5820
JiaG293 <giau32002@gmail.com>
2025-01-05 22:40:57 +0700

    ops(github-pages): remove workflow github pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-
+// basename={import.meta.env.BASE_URL}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
