5691f1a823d941db829158abce9e2cc460b061e0
JiaG293 <giau32002@gmail.com>
2025-01-06 10:29:04 +0700

    ops(github-pages): update workflow github pages

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
   base: '/',
 })
