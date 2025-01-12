3f2cdff00b0042b400360899e7cbca0fb4565784
JiaG293 <giau32002@gmail.com>
2025-01-12 19:46:40 +0700

    chore: change title app

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { Helmet } from "react-helmet";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Helmet>
-      <title>JiaG's Portfolio</title>
-    </Helmet>
     <BrowserRouter>
       <App />
     </BrowserRouter>
